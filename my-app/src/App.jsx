2c18f82b91bf249cd4ea77c307c8740049bab992
Artem <KorochanskyyArtem@hotmail.com>
2022-07-12 22:03:20 +0300

    My four commit

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,19 +1,12 @@
-import logo from './logo.svg';
+import logo from './images/Outdoor-Logo.webp';
 import styles from './App.module.css';
 
 function App() {
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
+    <div class={styles.container}>
+      <header class="relative w-full top-2.5 h-14.5px">
+        <img src={logo} class="relative top-2.5 left-4" alt="logo"/>
+
       </header>
     </div>
   );
