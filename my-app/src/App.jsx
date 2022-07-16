3c65640418af4ebdf6aa8cd2defc88f668e6a17a
Artem <KorochanskyyArtem@hotmail.com>
2022-07-16 22:24:11 +0300

    My five commit

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -6,7 +6,16 @@ function App() {
     <div class={styles.container}>
       <header class="relative w-full top-2.5 h-14.5px">
         <img src={logo} class="relative top-2.5 left-4" alt="logo"/>
-
+        <nav>
+          <ul class="flex">
+            <li><a href="#">Shop</a></li>
+            <li><a href="#">About us</a></li>
+            <li><a href="#">Our team</a></li>
+            <li><a href="#">Buyer's guides</a></li>
+            <li><a href="#">Categories</a></li>
+            <li><a href="#">Buy template</a></li>
+          </ul>
+        </nav>
       </header>
     </div>
   );
