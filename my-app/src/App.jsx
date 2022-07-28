ec43d046565ef1f71e13ff143746c3c64f9810ac
Artem <KorochanskyyArtem@hotmail.com>
2022-07-28 21:20:01 +0300

    My eleven commit

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -3,17 +3,18 @@ import styles from './App.module.css';
 function App() {
   return (
     <div class={styles.container}>
-      <header class="relative w-full top-2.5 h-14.5px">
+      <header class="relative flex w-full top-2.5 h-14.5px">
         <img src="src/images/Outdoor.webp" class="relative top-2.5 left-4" alt="logo"/>
         <nav>
-          <ul class="flex">
+          <ul class="flex text end">
             <li><a href="#">Shop</a></li>
             <li><a href="#">About us</a></li>
             <li><a href="#">Our team</a></li>
             <li><a href="#">Buyer's guides</a></li>
             <li><a href="#">Categories</a></li>
-            <li><a href="#">Buy template</a></li>
+            <li><button>Buy template</button></li>
           </ul>
+          <button><img src="src/images/Cart.webp" alt="cart" /></button>
         </nav>
       </header>
     </div>
