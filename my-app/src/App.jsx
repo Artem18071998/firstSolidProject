7515a9e8385afb24bcec43c08f8c055f2a4b403d
Artem <KorochanskyyArtem@hotmail.com>
2022-10-24 16:29:13 +0300

    My twelve commit

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,12 +1,14 @@
-import styles from './App.module.css';
+import logo from './images/Outdoor-Logo.png';
+import cart from './images/Cart.png';
+import styles from './App.module.css'; 
 
 function App() {
   return (
     <div class={styles.container}>
       <header class="relative flex w-full top-2.5 h-14.5px">
-        <img src="/images/Outdoor.webp" class="relative top-2.5 left-4" alt="logo"/>
-        <nav>
-          <ul class="flex text end">
+        <img src={logo} class="relative top-2.5 left-4" alt="logo"/>
+        <nav class="relative flex">
+          <ul class="">
             <li><a href="#">Shop</a></li>
             <li><a href="#">About us</a></li>
             <li><a href="#">Our team</a></li>
@@ -14,10 +16,10 @@ function App() {
             <li><a href="#">Categories</a></li>
             <li><button>Buy template</button></li>
           </ul>
-          <button class="bg-black"><img src="/images/Cart.webp" alt="cart"/></button>
+          <button class="bg-black"><img src={cart} alt="cart"/></button>
         </nav>
       </header>
-      <hero class="relative flex w-full background-image: ">
+      <hero class="relative flex w-full">
 
       </hero>
     </div>
--- my-app/src/index.jsx
+++ my-app/src/index.jsx
@@ -4,4 +4,5 @@ import "tailwindcss/tailwind.css";
 
 import "./index.css";
 import App from "./App";
+
 render(App, document.getElementById("root"));
