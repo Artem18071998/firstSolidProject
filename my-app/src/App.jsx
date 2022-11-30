003dbd428affd40ac6fefbadc39458a174fd0aee
Artem <KorochanskyyArtem@hotmail.com>
2022-11-30 18:44:53 +0200

    My eleven commit

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -5,24 +5,24 @@ import styles from './App.module.css';
 function App() {
   return (
     <div class={styles.container}>
-      <header class="relative flex w-full top-2.5 h-14.5px">
-        <img src={logo} class="relative top-2.5 left-4" alt="logo"/>
-        <nav class="relative flex">
-          <ul class="">
-            <li><a href="#">Shop</a></li>
-            <li><a href="#">About us</a></li>
-            <li><a href="#">Our team</a></li>
-            <li><a href="#">Buyer's guides</a></li>
-            <li><a href="#">Categories</a></li>
-            <li><button>Buy template</button></li>
+      <header class="relative flex justify-between items-center w-full top-2.5 h-[59px]">
+        <img src={logo} class="relative top-0.5 left-4" alt="logo"/>
+        <nav class="relative flex w-[802px] h-[59px]">
+          <ul class="relative flex">
+            <li class="pr-7 pl-3.5 pt-5 pb-[25px]"><a href="#">Shop</a></li>
+            <li class="pr-7 pt-5 pb-[25px]"><a href="#">About us</a></li>
+            <li class="pr-7 pt-5 pb-[25px]"><a href="#">Our team</a></li>
+            <li class="pr-[34px] pl-7 pt-5 pb-[25px]"><a href="#">Buyer's guides</a></li>
+            <li class="pr-10 pl-[34px] pb-5 pt-[19px]"><a href="#">Categories</a></li>
+            <li class="h-11 rounded-md bg-[#575445]"><button class="pl-[18px] pr-[18px] pt-2.5 pb-[11px] text-white">Buy template</button></li>
           </ul>
-          <button class="bg-black"><img src={cart} alt="cart"/></button>
+          <li class="relative left-5 flex h-11 rounded-md bg-[#575445] text-white"><button class="flex"><img class="pl-[18px] pt-[13px] pr-2 pb-3.5" src={cart} alt="cart"/>Cart<circle class="rounded-[9px] bg-[#ffffff] text-[#575445]">0</circle></button></li>
         </nav>
       </header>
       <hero class="relative flex w-full">
 
       </hero>
-    </div>
+      </div>
   );
 }
 
