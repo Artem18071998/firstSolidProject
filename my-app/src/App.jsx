67158571e16cef475c9c780f21f2403661975dbb
Artem <KorochanskyyArtem@hotmail.com>
2022-12-03 20:33:16 +0200

    My twelfth commit

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -7,16 +7,16 @@ function App() {
     <div class={styles.container}>
       <header class="relative flex justify-between items-center w-full top-2.5 h-[59px]">
         <img src={logo} class="relative top-0.5 left-4" alt="logo"/>
-        <nav class="relative flex w-[802px] h-[59px]">
-          <ul class="relative flex">
-            <li class="pr-7 pl-3.5 pt-5 pb-[25px]"><a href="#">Shop</a></li>
-            <li class="pr-7 pt-5 pb-[25px]"><a href="#">About us</a></li>
-            <li class="pr-7 pt-5 pb-[25px]"><a href="#">Our team</a></li>
-            <li class="pr-[34px] pl-7 pt-5 pb-[25px]"><a href="#">Buyer's guides</a></li>
-            <li class="pr-10 pl-[34px] pb-5 pt-[19px]"><a href="#">Categories</a></li>
-            <li class="h-11 rounded-md bg-[#575445]"><button class="pl-[18px] pr-[18px] pt-2.5 pb-[11px] text-white">Buy template</button></li>
+        <nav class="relative flex right-3.5 w-[802px] h-[59px]">
+          <ul class="text-primary-400 body-font font-poppins relative flex">
+            <li class="pr-7 pl-3.5 pt-5 pb-[25px] text-sm"><a href="#">Shop</a></li>
+            <li class="pr-7 pt-5 pb-[25px] text-base"><a href="#">About us</a></li>
+            <li class="pr-7 pt-5 pb-[25px] text-base"><a href="#">Our team</a></li>
+            <li class="relative right-[25px] pr-14px pl-7 pt-5 pb-[25px] text-sm"><a href="#">Buyer's guides</a></li>
+            <li class="relative right-5 pr-10 pl-[34px] pb-5 pt-[19px] text-lg"><a href="#">Categories</a></li>
+            <li class="relative top-[7px] right-[13px] h-11 rounded-md bg-[#575445] text-xl"><button class="pl-[18px] pr-[18px] pt-2.5 pb-[11px] text-white">Buy template</button></li>
           </ul>
-          <li class="relative left-5 flex h-11 rounded-md bg-[#575445] text-white"><button class="flex"><img class="pl-[18px] pt-[13px] pr-2 pb-3.5" src={cart} alt="cart"/>Cart<circle class="rounded-[9px] bg-[#ffffff] text-[#575445]">0</circle></button></li>
+          <li class="relative w-[125px] top-[7px] left-[7px] flex h-11 body-font font-poppins rounded-md bg-[#575445] text-white"><button class="flex text-xl"><img class="pl-[18px] pt-[13px] pr-2 pb-3.5" src={cart} alt="cart"/><span class="relative top-2.5">Cart</span><circle class="relative w-5 h-[18px] top-3.5 left-[11px] rounded-[9px] bg-[#ffffff] text-[#575445]">0</circle></button></li>
         </nav>
       </header>
       <hero class="relative flex w-full">
--- my-app/tailwind.config.js
+++ my-app/tailwind.config.js
@@ -3,7 +3,22 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    fontSize: {
+      sm: ['13px', '14px'],
+      base: ['14px', '14px'],
+      lg: ['13px', '20px'],
+      xl: ['16px', '22px'],
+    },
+    extend: {
+      colors: {
+        primary: {
+          400: '#444444;',
+        }
+      },
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif']
+      },
+    },
   },
   variants: {
     extend: {},
